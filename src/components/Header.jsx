0ea3dd16225437d1323c18fee81197f7847b7809
Manoj <msuthar1412@gmail.com>
2025-04-04 18:05:10 +0530

    Initial UI with header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>AI NFT Authenticator</h1>
+      <p>Verify and mint your NFT securely on Aptos</p>
+    </header>
+  );
+}
+
+export default Header;
